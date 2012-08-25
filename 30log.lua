c85014327bc9814fa99f91c23c403d27f8106185
Roland Yonaba <roland.yonaba@gmail.com>
2012-08-25 00:22:58 +0000

    Initial commit

---
+++ 30log.lua
@@ -0,0 +1,54 @@
+--[[
+	Copyright (c) 2012 Roland Yonaba
+
+	Permission is hereby granted, free of charge, to any person obtaining a
+	copy of this software and associated documentation files (the
+	"Software"), to deal in the Software without restriction, including
+	without limitation the rights to use, copy, modify, merge, publish,
+	distribute, sublicense, and/or sell copies of the Software, and to
+	permit persons to whom the Software is furnished to do so, subject to
+	the following conditions:
+
+	The above copyright notice and this permission notice shall be included
+	in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+	OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+	MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+	IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+	CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+	TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--]]
+
+local function deep_copy(t)
+	local r = {}
+	for k,v in pairs(t) do
+		if type(v) == 'table' then r[k] = deep_copy(v) else r[k] = v end
+	end
+	return r
+end
+
+local function instantiate(self,...)
+	local instance = setmetatable({},self)
+		if self.__init then
+			self.__init(instance, ...)
+		end
+	return instance
+end
+
+local function extends(self,extra_params)
+	local heirClass = class(extra_params)
+	heirClass.__index, heirClass.super = heirClass, self
+	return setmetatable(heirClass,self)
+end
+
+local baseMt = {__call = function (self,...) return self:new(...) end}
+class = function(members)
+	local newClass = members and deep_copy(members) or {}
+	newClass.__index, newClass.__call = newClass, baseMt.__call
+	newClass.new = instantiate
+	newClass.extends = extends
+	return setmetatable(newClass,baseMt)
+end
+
