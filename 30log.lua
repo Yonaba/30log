ddd3e825feeec4b5dfed864818de266963393837
Roland Yonaba <roland.yonaba@gmail.com>
2013-02-13 18:52:59 +0000

    tostringable objects __init can also be a table

--- 30log.lua
+++ 30log.lua
@@ -21,33 +21,33 @@
 	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --]]
 
-local type, pairs, setmetatable, class = type, pairs, setmetatable
-
-local function deep_copy(t)
-  local r = {}
+local type, pairs, setmetatable, baseMt, instances, classes, class = type, pairs, setmetatable, {}, {}, {}
+local function deep_copy(t, dest)
+  local r = dest or {}
   for k,v in pairs(t) do
     if type(v) == 'table' and k ~= "__index" then r[k] = deep_copy(v) else r[k] = v end
   end
   return r
 end
-
 local function instantiate(self,...)
-  local instance = setmetatable({},self)
-  if self.__init then self.__init(instance, ...) end
-  return instance
+  local instance = {} ; instances[instance] = tostring(instance)
+  if self.__init then
+		if type(self.__init) == 'table' then deep_copy(self.__init, instance) else self.__init(instance, ...) end
+	end
+  return setmetatable(instance,self)
 end
-
 local function extends(self,extra_params)
   local heirClass = class(extra_params)
   heirClass.__index, heirClass.super = heirClass, self
   return setmetatable(heirClass,self)
 end
-
-local baseMt = {__call = function (self,...) return self:new(...) end}
+baseMt = { __call = function (self,...) return self:new(...) end, __tostring = function(self,...)
+	if instances[self] then return ('object: <%s>'):format(instances[self]) end
+	return classes[self] and ('class: <%s>'):format(classes[self]) or self
+end}
 class = function(members)
-  local c = members and deep_copy(members) or {}
-  c.new, c.extends, c.__index, c.__call = instantiate, extends, c, baseMt.__call
-  return setmetatable(c,baseMt)
+  local c = members and deep_copy(members) or {} ; classes[c] = tostring(c)
+  c.new, c.extends, c.__index, c.__call, c.__tostring = instantiate, extends, c, baseMt.__call, baseMt.__tostring
+	return setmetatable(c,baseMt)
 end
-
 return class
