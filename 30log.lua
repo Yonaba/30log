99f24f80dabc0f6da270f0d01067c49011fee308
Roland Yonaba <roland.yonaba@gmail.com>
2013-02-14 09:18:41 +0000

    Named classes

--- 30log.lua
+++ 30log.lua
@@ -21,16 +21,16 @@
 	SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --]]
 
-local type, pairs, setmetatable, baseMt, instances, classes, class = type, pairs, setmetatable, {}, {}, {}
+local type, pairs, setmetatable, rawget, baseMt, _instances, _classes, class = type, pairs, setmetatable, rawget, {}, {}, {}
 local function deep_copy(t, dest)
-  local r = dest or {}
+  local t, r = t or {}, dest or {}
   for k,v in pairs(t) do
     if type(v) == 'table' and k ~= "__index" then r[k] = deep_copy(v) else r[k] = v end
   end
   return r
 end
 local function instantiate(self,...)
-  local instance = {} ; instances[instance] = tostring(instance)
+  local instance = {} ; _instances[instance] = tostring(instance)
   if self.__init then
 		if type(self.__init) == 'table' then deep_copy(self.__init, instance) else self.__init(instance, ...) end
 	end
@@ -42,11 +42,11 @@ local function extends(self,extra_params)
   return setmetatable(heirClass,self)
 end
 baseMt = { __call = function (self,...) return self:new(...) end, __tostring = function(self,...)
-	if instances[self] then return ('object: <%s>'):format(instances[self]) end
-	return classes[self] and ('class: <%s>'):format(classes[self]) or self
+	if _instances[self] then return ('object (of %s): <%s>'):format((rawget(getmetatable(self),'__name') or 'Unnamed'), _instances[self]) end
+	return _classes[self] and ('class (%s): <%s>'):format((rawget(self,'__name') or 'Unnamed'),_classes[self]) or self
 end}
-class = function(members)
-  local c = members and deep_copy(members) or {} ; classes[c] = tostring(c)
+class = function(attr)
+  local c = deep_copy(attr) ; _classes[c] = tostring(c)
   c.new, c.extends, c.__index, c.__call, c.__tostring = instantiate, extends, c, baseMt.__call, baseMt.__tostring
 	return setmetatable(c,baseMt)
 end
