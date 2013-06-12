0b63407fb5380d277d48c11e0462b45b4248e690
Yonaba <roland.yonaba@gmail.com>
2013-06-12 21:31:02 +0000

    Updated license header

--- 30logclasscommons.lua
+++ 30logclasscommons.lua
@@ -1,5 +1,5 @@
 --[[
-	Copyright (c) 2012 TsT <tst worldmaster fr>
+	Copyright (c) 2012-2013 TsT <tst worldmaster fr>
 
 	Permission is hereby granted, free of charge, to any person obtaining a
 	copy of this software and associated documentation files (the
