b136e2b13a2f003649d84e89af8cb9b125b4b5cc
Yonaba <roland.yonaba@gmail.com>
2013-12-29 10:16:10 +0000

    Switched to original implementation, now valid ad of 2c9acd5 changes)

--- 30logclasscommons.lua
+++ 30logclasscommons.lua
@@ -1,31 +1,12 @@
 local PATH = (...):match("^(.+)%.[^%.]+$") or ''
 local class = require(PATH .. ".30log")
 
-local function table_clone(t)
-  local copy = {}
-  for k,v in pairs(t) do
-    if type(v) == 'table' then
-      copy[k] = table_clone(v)
-    else
-      copy[k] = v
-    end
-  end
-  return copy
-end
-
-local function table_merge(dest, source)
-  for k,v in pairs(source) do  
-    dest[k] = v
-  end
-  return dest
-end
-
 -- Interface for cross class-system compatibility
 -- see https://github.com/bartbes/Class-Commons).
 if common_class ~= false and not common then
   common = {}
   function common.class(name, prototype, parent)
-    local klass = table_merge(class(parent), prototype)
+    local klass = class():extends(parent):extends(prototype)
     klass.__init = prototype.init or (parent or {}).init
     return klass
   end
