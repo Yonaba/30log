a26104e3a54c63ff2c227029aa965b5327807bf1
Yonaba <roland.yonaba@gmail.com>
2015-01-09 23:52:40 +0000

    Tabs to 2spaces

--- singleton.lua
+++ singleton.lua
@@ -3,16 +3,16 @@ local SingletonClass = class("Singleton")
 local instance = SingletonClass()
 
 function SingletonClass.new() 
-	error('Cannot instantiate from a Singleton class') 
+  error('Cannot instantiate from a Singleton class') 
 end
 function SingletonClass.init() end
 
 function SingletonClass.extend() 
-	error('Cannot extend from a Singleton class')
+  error('Cannot extend from a Singleton class')
 end
 
 function SingletonClass:getInstance()
-	return instance
+  return instance
 end
 
 return SingletonClass
