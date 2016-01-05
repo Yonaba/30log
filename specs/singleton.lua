84979798ccfaa004c536b961ab6bd10f413dfacf
Yonaba <roland.yonaba@gmail.com>
2016-01-05 09:52:52 +0000

    Added singleton specs

---
+++ specs/singleton.lua
@@ -0,0 +1,41 @@
+require 'luacov'
+local SingletonClass = require 'singleton'
+local class = require "30log"
+
+context('30log is distributed with a default singleton class implementation', function()
+
+	context('This SingletonClass', function()
+	
+		local singletonInstance
+		
+		before(function()
+			singletonInstance = SingletonClass:getInstance()
+		end)
+
+		test('is recognized as a class', function()
+			assert_true(class.isClass(SingletonClass))
+		end)
+		
+		test('is named "Singleton"', function()
+			assert_equal(SingletonClass.name, "Singleton")
+		end)
+		
+		test('this class cannot instantiate', function()
+			assert_error(SingletonClass)
+		end)
+		
+		test('and cannot be derived to make new classes.', function()
+			assert_error(SingletonClass.extend)
+		end)
+		
+		test('It implements a method named "getInstance"', function()
+			assert_equal(type(SingletonClass.getInstance), "function")
+		end)
+		
+		test('which returns a private local instance of the SingletonClass', function()
+			assert_true(class.isInstance(singletonInstance, SingletonClass))
+		end)		
+		
+	end)
+
+end)
