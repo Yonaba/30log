24fedf4b668c5370e71ce9e1ce585ae76c5d2350
Yonaba <roland.yonaba@gmail.com>
2016-09-27 00:31:46 +0000

    Move from Lua 5.3.2 to Lua 5.3.3

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -67,8 +67,8 @@ else
     curl http://www.lua.org/ftp/lua-5.2.4.tar.gz | tar xz
     cd lua-5.2.4;
   elif [ "$LUA" == "lua5.3" ]; then
-    curl http://www.lua.org/ftp/lua-5.3.2.tar.gz | tar xz
-    cd lua-5.3.2;
+    curl http://www.lua.org/ftp/lua-5.3.3.tar.gz | tar xz
+    cd lua-5.3.3;
   fi
 
   # Build Lua without backwards compatibility for testing
@@ -118,5 +118,5 @@ elif [ "$LUA" == "lua5.1" ]; then
 elif [ "$LUA" == "lua5.2" ]; then
   rm -rf lua-5.2.4;
 elif [ "$LUA" == "lua5.3" ]; then
-  rm -rf lua-5.3.2;
+  rm -rf lua-5.3.3;
 fi
